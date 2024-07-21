0f70e9d3f4ec70c7ddc38a9c241ee8bc43d9892c
Mack Octavian <maestrooctavian@gmail.com>
2024-07-21 22:57:42 +0300

    Added a simple validation to textarea

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 export default function TextArea({ word, setWord }) {
+  const [showWarning, setShowWarning] = useState(false);
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    if (newText.includes("<script>")) {
+      //alert("No script tag allowed");
+      newText = newText.replace("<script>", "");
+      setShowWarning(true);
+    }
+    setWord(newText);
+  };
+
   return (
-    <textarea
-      className="textarea"
-      onChange={(e) => {
-        setWord(e.target.value);
-      }}
-      placeholder="Enter your text"
-      spellCheck="false"
-      value={word}
-    ></textarea>
+    <div className="textarea">
+      <textarea
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+        value={word}
+      ></textarea>
+      {showWarning && <Warning />}
+    </div>
   );
 }
