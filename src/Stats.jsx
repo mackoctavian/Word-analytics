f31ab910c2d3035cfac92818e1ece852b69c14de
Mack Octavian <maestrooctavian@gmail.com>
2024-07-21 23:42:40 +0300

    Made a reusable component stat

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,22 +1,19 @@
 export default function Stats({ word }) {
   return (
     <section className="stats">
-      <section className="stat">
-        <span className="stat__number">0</span>
-        <h2 className="second-heading">Words</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">0</span>
-        <h2 className="second-heading">Characters</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">280</span>
-        <h2 className="second-heading">Instagram</h2>
-      </section>
-      <section className="stat">
-        <span className="stat__number">2200</span>
-        <h2 className="second-heading">Facebook</h2>
-      </section>
+      <Stat number="0" name="words"></Stat>
+      <Stat number="0" name="characters"></Stat>
+      <Stat number="280" name="Instagram"></Stat>
+      <Stat number="2200" name="Facebook"></Stat>
+    </section>
+  );
+}
+
+function Stat({ number, name }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{number}</span>
+      <h2 className="second-heading">{name}</h2>
     </section>
   );
 }
